c5457aa3e8d2bd1d82c3fe567068bce7f75fcc9b
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-10-15 07:52:49 +0530

    deleted uneccessary comment

--- code/C and c++ code/kernel.c
+++ code/C and c++ code/kernel.c
@@ -1,6 +1,6 @@
 #include "include/mystdio.h"
 
-void kernelmain()     //we do not use void or int or float or anything because it is already declared in kernel.asm
+void kernelmain()
 {
     myprintf("hello world ");
 }
