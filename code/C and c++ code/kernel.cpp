613a14edce120f73d4bfc1a25d30097e69916fcf
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-10-17 11:24:14 +0530

    another random commit

---
+++ code/C and c++ code/kernel.cpp
@@ -0,0 +1,9 @@
+#include "include/mystdio.h"
+
+//the extern c is there to tell the compiler that this is a c 
+//function and not a cpp function, it wont compile without it
+extern "C" void kernelmain()
+{
+    clearScreen();
+    myprintf("hello world\n");
+}
