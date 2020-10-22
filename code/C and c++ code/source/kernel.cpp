9dd20d857c6bb62d05a0128ba23fc25d92932932
ishaan-bose <ishaan.varshney.bose@gmail.com>
2020-10-22 11:36:50 +0530

    some commit

--- code/C and c++ code/source/kernel.cpp
+++ code/C and c++ code/source/kernel.cpp
@@ -1,9 +1,10 @@
-#include "include/mystdio.h"
+#include "mystdio.h"
 
 //the extern c is there to tell the compiler that this is a c 
 //function and not a cpp function, it wont compile without it
 extern "C" void kernelmain()
 {
     clearScreen(); //clear the screen first
-    printf("hello world\n");
+    //the (string) part is not needed but avoids a warning the compiler gives
+    printf((string)"hello world\n") ;
 }
